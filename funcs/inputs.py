ceed1aa180a805d2b22f3737d840735a9c1aaea0
Alby <42292129+albyp@users.noreply.github.com>
2025-02-22 07:55:26 +0800

    Add mission naming functions

--- funcs/inputs.py
+++ funcs/inputs.py
@@ -18,3 +18,27 @@ def new_lidar_mission(window) -> None:
     pg.click()
     time.sleep(0.1)
     return
+
+def name_mission(mission:str) -> None:
+    """Clear the default mission name and input the given parameter."""
+    logging.debug("Clearing mission name")
+    pg.hotket('ctrl', 'a')
+    time.sleep(0.1)
+    pg.press('backspace')
+    logging.debug(f"Inputting mission name: '{mission}'.")
+    pg.write(mission)
+    return
+
+def input_mission_data(data_dir:str) -> None:
+    """Paste the input mission data directory for processing.
+    Parameters
+    data_dir (str) : Directory for LiDAR data
+    """
+    logging.debug("Inputting mission data to DJI Terra.")
+    pg.write(data_dir)
+    time.sleep(0.1)
+    pg.press('enter')
+    time.sleep(0.1)
+    pg.press('enter')
+    logging.info('Finishing inputting LiDAR data to DJI Terra')
+    return
