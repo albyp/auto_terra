8dd6aa05338181e8bdd787b778598d0b82d9a6d4
Alby <42292129+albyp@users.noreply.github.com>
2025-02-22 15:25:44 +0800

    Upload example config

---
+++ example.config.py
@@ -0,0 +1,8 @@
+MISSION_NAME = "YYMMDD-ProjectName_LiDAR"
+MISSION_DATA_DIR = "C:\\Pix4D\\YYMMDD-ProjectName\\1. LiDAR"
+EPSG = "28350" # EPSG code for coordinate system
+BASE_COORDINATES = {
+    "x": "420550.295",
+    "y": "6302560.234",
+    "z": "301.750"
+}
