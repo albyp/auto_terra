86e80a825221a9fe545823dd494bc911caf06225
Alby <42292129+albyp@users.noreply.github.com>
2025-02-23 09:56:36 +0800

    Integrate batch processing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyautogui as pg
 
 # Import window functions
 from funcs.window import get_terra_window, focus_terra
-from funcs.inputs import new_lidar_mission, name_mission, input_mission_data
+from funcs.inputs import new_lidar_mission, name_mission, input_mission_data, start_processing
 from funcs.base_settings import select_base_settings, change_coordinate_system, input_coordinates
 from funcs.parameters import modify_parameters
 
@@ -16,6 +16,7 @@ logging.basicConfig(
 
 # Defaults for testing
 from config import MISSION_NAME, MISSION_DATA_DIR, EPSG, BASE_COORDINATES
+from config import MISSIONS
 PROCESS_BASE = True
 
 def get_initial_mouse_pos() -> tuple:
@@ -52,21 +53,25 @@ def main() -> None:
     window = get_terra_window()
     focus_terra(window)
 
-    # Create new mission and set up for import
-    new_lidar_mission(window)
-    name_mission(MISSION_NAME, window)
+    for mission in MISSIONS:
+        focus_terra(window)
 
-    # Import LiDAR data to project
-    input_mission_data(MISSION_DATA_DIR, window)
-    
-    # Process base settings
-    if PROCESS_BASE:
-        logging.info("Executing process base functions...")
-        select_base_settings(window)
-        change_coordinate_system(EPSG, window)
-        input_coordinates(BASE_COORDINATES, window)
+        # Create new mission and set up for import
+        new_lidar_mission(window)
+        name_mission(mission[0], window)
 
-    modify_parameters(window)
+        # Import LiDAR data to project
+        input_mission_data(mission[1], window)
+        
+        # Process base settings
+        if PROCESS_BASE:
+            logging.info("Executing process base functions...")
+            select_base_settings(window)
+            change_coordinate_system(EPSG, window)
+            input_coordinates(BASE_COORDINATES, window)
+
+        modify_parameters(window)
+        start_processing(window, auto_accept=True)
 
     return_mouse_pos(initial_mouse_pos)
     return
