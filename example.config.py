2a18942711b963ee87c2cf72a7f8dd45f097eb60
Alby <42292129+albyp@users.noreply.github.com>
2025-02-24 08:49:22 +0800

    Update example config

--- example.config.py
+++ example.config.py
@@ -1,13 +1,22 @@
-MISSION_NAME = "YYMMDD-ProjectName_LiDAR"
-MISSION_DATA_DIR = "C:\\Pix4D\\YYMMDD-ProjectName\\1. LiDAR"
-EPSG = "28350" # EPSG code for coordinate system
+# Determine whether to auto accept the processing task once inputs are complete
+AUTO_ACCEPT = False
+
+# Base coordinate EPSG code
+EPSG = "28350"
+
+# Base coordinates (for RTK base used with drone)
 BASE_COORDINATES = {
     "x": "420550.295",
     "y": "6302560.234",
     "z": "301.750"
 }
 
+# Mission inputs, use single line for single mission
 MISSIONS = [
     ['YYMMDD-ProjectName', 'C:\\Pix4D\\YYMMDD-ProjectName\\1. LiDAR'],
     ['YYMMDD-ProjectName2', 'C:\\Pix4D\\YYMMDD-ProjectName2\\1. LiDAR']
 ]
+
+# Depreciated, use `MISSIONS` list instead
+MISSION_NAME = "YYMMDD-ProjectName_LiDAR"
+MISSION_DATA_DIR = "C:\\Pix4D\\YYMMDD-ProjectName\\1. LiDAR"
